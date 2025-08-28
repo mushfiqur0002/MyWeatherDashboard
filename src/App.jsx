693b0413c133705167d231f83020d83e1ce96a43
mushfiqur0002 <mushfiqur.uk@outlook.com>
2025-08-28 17:45:57 +0100

    Fresh beginning

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+     <h1 class="text-8xl font-bold underline">
+        Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App;
